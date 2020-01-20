ddc6a67a5e08f27128fe840befd57e07e335f831
Adrian Danemayer <adanemayer@protonmail.com>
2020-01-19 20:42:15 -0500

    Copy selected ovpn and rename

--- vpn_rotate.sh
+++ vpn_rotate.sh
@@ -3,16 +3,20 @@
 # Obtain list of OpenVPN config files in user directory
 VPN_GW_LIST=( `find $HOME/vpn/sites/ -name '*.ovpn'` )
 
-echo $VPN_GW_LIST
+# echo $VPN_GW_LIST
 
 # Obtain the number of elements in the array
 RANGE=${#VPN_GW_LIST[@]}
-echo "$RANGE"
+# echo "$RANGE"
 
 # Randomly select an element in the list
 let "NUMBER = RANDOM % $RANGE"
-echo "$NUMBER"
+# echo "$NUMBER"
 # Print name of selected file
-echo ${VPN_GW_LIST[$NUMBER]}
+echo "Selecting ${VPN_GW_LIST[$NUMBER]} for new tunnel"
+
+cp ${VPN_GW_LIST[$NUMBER]} "$HOME/vpn/temp/client.conf"
+
+ls $HOME/vpn/temp
 
 # ls /home/adrian
