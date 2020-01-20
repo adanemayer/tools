9ba2caa64b111b38b7d62f72b4c909ab500b67ea
Adrian Danemayer <adanemayer@protonmail.com>
2020-01-20 13:37:25 -0500

    Working script from Pi

--- vpn_rotate.sh
+++ vpn_rotate.sh
@@ -3,14 +3,14 @@
 # Requirements
 # 1. Create /var/log/vpn_rotate.log, chmod 777
 # 2. All OpenVPN files need to be changed to user file for username/password
-# 3. All OpenVPN files need to be placed in Home/vpn/sites directory
+# 3. All OpenVPN files need to be placed in /home/root/vpn/sites directory
 
 # Log current IP
 CUR_IP=( `curl ifconfig.me` )
-echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: Current IP address is $CUR_IP" > /var/log/vpn_rotate.log
+echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: Current IP address is $CUR_IP" >> /var/log/vpn_rotate.log
 
 # Obtain list of OpenVPN config files in user directory
-VPN_GW_LIST=( `find $HOME/vpn/sites/ -name '*.ovpn'` )
+VPN_GW_LIST=( `find /home/pi/vpn/sites/ -name '*.ovpn'` )
 
 # Obtain the number of elements in the array
 RANGE=${#VPN_GW_LIST[@]}
@@ -19,22 +19,16 @@ RANGE=${#VPN_GW_LIST[@]}
 let "NUMBER = RANDOM % $RANGE"
 
 # Log name of selected file
-echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: Selecting ${VPN_GW_LIST[$NUMBER]} for new tunnel" > /var/log/vpn_rotate.log
+echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: Selecting ${VPN_GW_LIST[$NUMBER]} for new tunnel" >> /var/log/vpn_rotate.log
 
 # Copy the selected OpenVpn file to the OpenVPN working directory and rename it
 cp ${VPN_GW_LIST[$NUMBER]} "/etc/openvpn/client.conf"
 
-# Get OpenVPN service status
-systemctl status openvpn
-
 # Restart OpenVPN service
-# systemctl restart openvpn
+systemctl restart openvpn
 
 sleep 30
 
-# Check OpenVPN service status
-systemctl status openvpn
-
 # Check new Public IP
 NEW_IP=( `curl ifconfig.me` )
-echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: New IP address is $NEW_IP" > /var/log/vpn_rotate.log
+echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: New IP address is $NEW_IP" >> /var/log/vpn_rotate.log
