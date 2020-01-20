7e2c100765fc5a4c0ffe4975452a5de1e9c1481a
Adrian Danemayer <adanemayer@protonmail.com>
2020-01-20 12:09:45 -0500

    Add comments, OpenVPN controls, ip check

--- vpn_rotate.sh
+++ vpn_rotate.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Requirements
+# 1. Create /var/log/vpn_rotate.log, chmod 777
+# 2. All OpenVPN files need to be changed to user file for username/password
+# 3. All OpenVPN files need to be placed in Home/vpn/sites directory
+
+
 # Obtain list of OpenVPN config files in user directory
 VPN_GW_LIST=( `find $HOME/vpn/sites/ -name '*.ovpn'` )
 
-# echo $VPN_GW_LIST
-
 # Obtain the number of elements in the array
 RANGE=${#VPN_GW_LIST[@]}
-# echo "$RANGE"
 
 # Randomly select an element in the list
 let "NUMBER = RANDOM % $RANGE"
-# echo "$NUMBER"
-# Print name of selected file
-echo "Selecting ${VPN_GW_LIST[$NUMBER]} for new tunnel"
 
-cp ${VPN_GW_LIST[$NUMBER]} "$HOME/vpn/temp/client.conf"
+# Log name of selected file
+echo "VPN_ROTATE - `date +"%Y-%m-%d_%T"` - INFO: Selecting ${VPN_GW_LIST[$NUMBER]} for new tunnel" > /var/log/vpn_rotate.log
 
-ls $HOME/vpn/temp
+# Copy the selected OpenVpn file to the OpenVPN working directory and rename it
+cp ${VPN_GW_LIST[$NUMBER]} "/etc/openvpn/client.conf"
 
-# ls /home/adrian
+# Get OpenVPN service status
+systemctl status openvpn
+
+# Restart OpenVPN service
+# systemctl restart openvpn
+
+sleep 60
+
+# Check OpenVPN service status
+systemctl status openvpn
+
+# Check new Public IP
+curl ifconfig.me
